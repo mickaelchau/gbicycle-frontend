1c4b90eabb1cc1645abb5c24b7fca1c6aa241b00
Mickaël Chau <mickaelchau@google.com>
2023-02-02 09:28:54 +0000

    My frontend is fetching the endpoint

---
+++ fetch_entity.js
@@ -0,0 +1,11 @@
+console.log("heloo")
+fetch('https://get-datastore-entity-r4yqmr3v3q-uc.a.run.app/data/customer1')
+  .then(response => response.text())
+  .then(data => {
+    // use the data to do something in the front-end
+    console.log(data);
+    document.getElementById("fetchOutput").textContent = data;
+  })
+  .catch(err => {
+    console.error('Error retrieving data:', err);
+  });
