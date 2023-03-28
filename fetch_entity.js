fb9505c8782416405b7b30356f376ac3aede4eae
Mickael Chau <mickaelchau@lhuillier.altostrat.com>
2023-03-28 16:09:12 +0000

    using the async await syntax

--- fetch_entity.js
+++ fetch_entity.js
@@ -1,11 +1,12 @@
-console.log("heloo")
-fetch('https://get-datastore-entity-r4yqmr3v3q-uc.a.run.app/data/customer1')
-  .then(response => response.text())
-  .then(data => {
-    // use the data to do something in the front-end
-    console.log(data);
-    document.getElementById("fetchOutput").textContent = data;
-  })
-  .catch(err => {
-    console.error('Error retrieving data:', err);
-  });
+async function fetchData() {
+    try {
+        const response = await fetch('https://get-datastore-entity-r4yqmr3v3q-uc.a.run.app/data/customer1');
+        const data = await response.text();
+        console.log(data);
+        document.getElementById("fetchOutput").textContent = data;
+    } catch (err) {
+        console.error('Error retrieving data:', err);
+    }
+}
+
+fetchData();
