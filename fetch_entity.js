d0683fa3d65f28bf11471e5f9debb3ddbf31efef
mickaelgoogle <123967769+mickaelgoogle@users.noreply.github.com>
2023-06-20 17:35:29 +0200

    Update fetch_entity.js

--- fetch_entity.js
+++ fetch_entity.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
-async function fetchData() {
+async function fetchData(bicycleId) {
     try {
-        const response = await axios.get('https://gbicycle-backend-kqs5kdn4xa-uc.a.run.app/data/1');
+        const response = await axios.get('https://gbicycle-backend-kqs5kdn4xa-uc.a.run.app/data/' + bicycleId);
         console.log(response.data);
         return response.data;
         //document.getElementById("fetchOutput").textContent = data;
